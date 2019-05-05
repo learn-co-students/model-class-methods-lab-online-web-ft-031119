92c898027994b937e96f4c20dfa8aa030742eace
daschne8 <daschne8@gmail.com>
2019-05-05 12:17:59 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,33 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.all[0,5]
+  end
+
+  def self.dinghy
+    Boat.where("length < 20")
+  end
+
+  def self.ship
+    Boat.where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    Boat.all.sort_by{|boat| boat.name}[-3,Boat.all.count].reverse
+  end
+
+  def self.without_a_captain
+    Boat.where(captain: [nil,""])
+  end
+
+  def self.sailboats
+    Boat.all.select{|boat| boat.classifications.pluck(:name).include?("Sailboat")}
+  end
+
+  def self.with_three_classifications
+    Boat.all.select{|boat| boat.classifications.count == 3}
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,27 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def classifications_names
+    self.boats.collect{|boat| boat.classifications}.flatten.uniq.pluck(:name)
+  end
+
+  def self.catamaran_operators
+    boats = Boat.all.select{|boat| boat.classifications.pluck(:name).include?("Catamaran") && boat.captain}
+    boats.collect{|boat| boat.captain}.uniq
+  end
+
+  def self.sailors
+    boats = Boat.all.select{|boat| boat.classifications.pluck(:name).include?("Sailboat") && boat.captain}
+    boats.collect{|boat| boat.captain}.uniq
+  end
+
+  def self.talented_seafarers
+    cs1 = Captain.all.select{|captain| captain.classifications_names.include?("Sailboat")}
+    cs2 = Captain.all.select{|captain| captain.classifications_names.include?("Motorboat")}
+    cs = cs1 & cs2
+  end
+
+  def self.non_sailors
+    Captain.all.select{|captain| !captain.classifications_names.include?("Sailboat")}
+  end
 end
